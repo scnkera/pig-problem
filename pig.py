fdc943cfb09738b90ea0fbcecd7c811d3c356b7a
Christelle Nkera <scnkera@gmail.com>
2025-01-13 19:37:48 -0500

    Updated pig_latin func

--- pig.py
+++ pig.py
@@ -1,8 +1,30 @@
 def pig_latin(sentence):
     # Write your solution here!
-    pass
+    # word_list = []
 
-# Test cases
+    word_list = sentence.split()
+    new_word_list = []
+
+    for word in word_list:
+        new_word_list.append(word_update(word))
+
+    response = " ".join(new_word_list)
+
+    return response
+
+
+def word_update(word):
+    if word[0] in 'aeiou':
+        return word
+
+    new_word = f'{word[1:]}{word[0]}ay'
+
+    return new_word
+
+
+# pig_latin("something")
+
+# # Test cases
 assert pig_latin("something") == "omethingsay"
 assert pig_latin("awesome") == "awesome"
 assert pig_latin("latin is a hard language") == "atinlay is a ardhay anguagelay"
